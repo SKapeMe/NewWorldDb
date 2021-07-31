1ba60591d4cc5a13cabd31b614cd731508377df9
SKape <simon.keltsch@pb-keltsch.de>
2021-07-31 14:58:12 +0200

    Grundlagen

--- New_World/New_World/Material.cs
+++ New_World/New_World/Material.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using New_World.db;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,35 @@ namespace New_World
 {
     class Material
     {
+        public int id { get; set; }
+        public string bezeichnung { get; set; }
+
+        public Material()
+        {
+
+        }
+
+        public Material(int id)
+        {
+            this.id = id;
+        }
+
+        public Material(int id, string bezeichnung)
+        {
+            this.id = id;
+            this.bezeichnung = bezeichnung;
+        }
+
+        public static Material Lesen(int id)
+        {
+            try
+            {
+                return MaterialDB.Einzellesen(id);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
